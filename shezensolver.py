329decda3af28892f4960c6cf001963e55a3a1c8
R-o-b-o <39059788+R-o-b-o@users.noreply.github.com>
2021-11-24 16:22:25 +0000

    added symbol recognition

--- shezensolver.py
+++ shezensolver.py
@@ -1,11 +1,10 @@
 import pyautogui
 import pytesseract
 import numpy as np
-from PIL import Image, ImageGrab
+from PIL import Image, ImageGrab, ImageDraw
 
 
-def is_blank(image):
-    
+def is_blank_section(image):
     background_color = (202, 202, 189)
     pixels = image.getcolors(image.width * image.height)
 
@@ -17,6 +16,24 @@ def is_blank(image):
 
     return nearness_count/(image.width * image.height) > 0.9
 
+def recognize_symbol_col(symbol_im):
+    pixels = symbol_im.getcolors(symbol_im.width * symbol_im.height)
+
+    symbol_colors = [
+        [(174, 44, 20), (174, 43, 19)],
+        [(18, 110, 75)],
+        [(0, 0, 0), (14, 15, 13)]
+    ]
+
+    for _, color in pixels:
+        for i in range(len(symbol_colors)):
+            try:
+                symbol_col = symbol_colors[i].index(color[:3])
+                return symbol_colors[i][0]
+            except ValueError:
+                pass
+
+       
 
 def get_screen_grab():
     return Image.open('example.png')
@@ -31,6 +48,18 @@ def show_board(board):
 
     board_im.show()
 
+def show_board_symbol_recognition(board):
+    max_col = max([len(x) for x in board])
+    board_im = Image.new("RGB", tuple(np.array((len(board), max_col))*shape_size))
+
+    for col_index, col in enumerate(board):
+        for row_index, square in enumerate(col):
+            d = ImageDraw.Draw(square)
+            d.text((0,0), recognize_symbol(square), fill=recognize_symbol_col(square))
+            board_im.paste(square, tuple(np.array((col_index, row_index))*shape_size))
+
+    board_im.show()
+
 def get_board_im_list(screen_grab):
     board_im_list = []
     for i in range(8):
@@ -40,7 +69,7 @@ def get_board_im_list(screen_grab):
         while True:
             square_im = screen_grab.crop(tuple(coords)+tuple(coords+shape_size))
 
-            if is_blank(square_im):
+            if is_blank_section(square_im):
                 break
 
             col.append(square_im)
@@ -49,9 +78,17 @@ def get_board_im_list(screen_grab):
         board_im_list.append(col)
     return board_im_list
 
+def recognize_symbol(symbol_im):
+    # symbol_im.show()
+    symbol = pytesseract.image_to_string(symbol_im, config="--psm 13")[0]
+    # print(symbol)
+    symbol = symbol.replace("g","9")
+    return symbol if symbol in "123456789" else "D"
+    
+
 
 starting_coords = np.array((408, 397))
-shape_size = np.array((25, 25))
+shape_size = np.array((25, 20))
 x_offset = 152
 y_offset = 31
 
@@ -59,5 +96,10 @@ if __name__ == "__main__":
     screen_grab = get_screen_grab()
     board_im_list = get_board_im_list(screen_grab)
 
-    show_board(board_im_list)
+    # show_board(board_im_list)
+    show_board_symbol_recognition(board_im_list)
+
+    for col in board_im_list:
+        for symbol_im in col:
+            recognize_symbol(symbol_im)
 
